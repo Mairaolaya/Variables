b718d5ae5d2e258b0811e65d12dd7572f0c62511
Maira <110685055+Mairaolaya@users.noreply.github.com>
2022-10-25 15:39:13 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+
+
+public class Main {
+    static final double pi = 2.9;
+    public static void main(String[] args) {
+        System.out.println("Hellooooo!");
+        String nombre = "Maira";
+        System.out.println("su nombre es: " + nombre);
+         nombre = "Maira olaya";
+        System.out.println("su nombre completo es: " + nombre);
+        System.out.println( nombre + " Hagamos operaciones sencillas con los numeros 3 y 4");
+        int a= 3;
+        int b=4;
+        System.out.println( "La suma: " + (a + b) );
+        System.out.println("multiplicacion: "+ (a * b));
+        System.out.println("Division :" + (a / b));
+        System.out.println("ahora");
+        System.out.println("Boleean de m: ");
+        boolean m;
+        m=false;
+        System.out.println("m falso: " + m);
+        m = true;
+        System.out.println("m verdadero: " + m);
+
+
+
+
+
+
+
+    }
+}
